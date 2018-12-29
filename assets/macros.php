40381dca68db88b92df6756141eef7d7fe56d342
mikeeR23 <41097168+mikeeR23@users.noreply.github.com>
2018-12-29 02:45:13 -0500

    Update macros.php

--- assets/macros.php
+++ assets/macros.php
@@ -14,8 +14,8 @@
 	// Mysql info
 	$database = 'workoutapp';
 	$server = 'localhost';
-	$username = 'root';
-	$password = 'Dg464569!';
+	$username = '';
+	$password = '';
 
 	$conn = new mysqli($server, $username, $password, $database);
 
